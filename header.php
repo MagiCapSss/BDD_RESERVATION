ea960c456fc95145d7a1750e8f37184111284a06
Magicaps <122777700+MagiCapSss@users.noreply.github.com>
2025-02-26 20:15:05 +0100

    Add footer and header

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Système de Réservation</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <header class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Réservation en Ligne</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="reservation.php">Prendre un rendez-vous</a></li>
+                    <li class="nav-item"><a class="nav-link" href="profil.php">Mon profil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="connexion.php">Connexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
